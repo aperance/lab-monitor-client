de8145b64bea166365d1afa6b944456ada488394
William Aperance <aperance@gmail.com>
2018-03-03 01:24:52 -0800

    Cleanup

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -5,81 +5,68 @@ import ToolbarItem from "./ToolbarItem.js";
 
 class Toolbar extends Component {
   render() {
-    console.log(this.props.rows.length);
-
     return (
-      <div>
-        {" "}
-        <List>
-          {this.props.rows.length === 1 && (
-            <div>
-              <ToolbarItem
-                name="State"
-                icon="list"
-                chevron={true}
-                selected={this.props.view === "statePage"}
-                onClick={e => this.props.handleClick(e, "statePage")}
-              />
-              <ToolbarItem
-                name="Logs"
-                icon="description"
-                chevron={true}
-                selected={this.props.view === "logsPage"}
-                onClick={e => this.props.handleClick(e, "logsPage")}
-              />
-              <ToolbarItem
-                name="History"
-                icon="history"
-                chevron={true}
-                selected={this.props.view === "history"}
-                onClick={e => this.props.handleClick(e, "history")}
-              />
-              <Divider />
-              <ToolbarItem
-                name="PSTools"
-                icon="laptop_windows"
-                chevron={true}
-                selected={this.props.view === "psTools"}
-                onClick={e => this.props.handleClick(e, "psTools")}
-              />
-            </div>
-          )}
-
-          <ToolbarItem
-            name="Log Level"
-            icon="settings"
-            chevron={true}
-            selected={this.props.view === "logLevel"}
-            onClick={e => this.props.handleClick(e, "logLevel")}
-          />
-          <Divider />
-          <ToolbarItem
-            name="Delete Logs"
-            icon="delete"
-            chevron={false}
-            selected={false}
-            onClick={e =>
-              this.props.requestAction(this.props.rows, "deleteLogs")
-            }
-          />
-          <ToolbarItem
-            name="Clean Start"
-            icon="refresh"
-            chevron={false}
-            selected={false}
-            onClick={e =>
-              this.props.requestAction(this.props.rows, "cleanStart")
-            }
-          />
-          <ToolbarItem
-            name="RAM Clear"
-            icon="memory"
-            chevron={false}
-            selected={false}
-            onClick={e => this.props.requestAction(this.props.rows, "ramClear")}
-          />
-        </List>
-      </div>
+      <List>
+        {this.props.rows.length === 1 && (
+          <div>
+            <ToolbarItem
+              name="State"
+              icon="list"
+              chevron={true}
+              selected={this.props.view === "statePage"}
+              onClick={e => this.props.handleClick(e, "statePage")}
+            />
+            <ToolbarItem
+              name="Logs"
+              icon="description"
+              chevron={true}
+              selected={this.props.view === "logsPage"}
+              onClick={e => this.props.handleClick(e, "logsPage")}
+            />
+            <ToolbarItem
+              name="History"
+              icon="history"
+              chevron={true}
+              selected={this.props.view === "history"}
+              onClick={e => this.props.handleClick(e, "history")}
+            />
+            <Divider />
+            <ToolbarItem
+              name="PSTools"
+              icon="laptop_windows"
+              chevron={true}
+              selected={this.props.view === "psTools"}
+              onClick={e => this.props.handleClick(e, "psTools")}
+            />
+          </div>
+        )}
+        <ToolbarItem
+          name="Log Level"
+          icon="settings"
+          chevron={true}
+          selected={this.props.view === "logLevel"}
+          onClick={e => this.props.handleClick(e, "logLevel")}
+        />
+        <Divider />
+        <ToolbarItem
+          name="Delete Logs"
+          icon="delete"
+          chevron={false}
+          onClick={e => this.props.requestAction(this.props.rows, "deleteLogs")}
+        />
+        <ToolbarItem
+          name="Clean Start"
+          icon="refresh"
+          chevron={false}
+          onClick={e => this.props.requestAction(this.props.rows, "cleanStart")}
+        />
+        <ToolbarItem
+          name="RAM Clear"
+          icon="memory"
+          chevron={false}
+          onClick={e => this.props.requestAction(this.props.rows, "ramClear")}
+        />
+      </List>
     );
   }
 }
