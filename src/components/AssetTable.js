c28c4ac80dba6d817a4c42831370abc44064740f
William Aperance <aperance@gmail.com>
2018-03-03 01:47:36 -0800

    Cleanup

--- src/components/AssetTable.js
+++ src/components/AssetTable.js
@@ -9,40 +9,37 @@ import Table, {
 class AssetTable extends Component {
   render() {
     return (
-      <div style={{ whiteSpace: "nowrap" }}>
-        <Table>
-          <TableHead>
-            <TableRow style={{ height: "48px" }}>
+      <Table style={{ whiteSpace: "nowrap" }}>
+        <TableHead>
+          <TableRow style={{ height: "48px" }}>
+            {this.props.columns.map(column => (
+              <TableCell key={column.title} style={{ paddingRight: "12px" }}>
+                {column.title}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(this.props.tableData).map(rowKey => (
+            <TableRow
+              hover
+              onClick={e => this.props.handleRowClick(e.nativeEvent, rowKey)}
+              key={rowKey}
+              selected={this.props.selected.includes(rowKey)}
+              style={{ height: "36px" }}
+            >
               {this.props.columns.map(column => (
-                <TableCell key={column.title} style={{ paddingRight: "12px" }}>
-                  {column.title}
+                <TableCell
+                  key={rowKey + column.property}
+                  style={{ fontSize: "0.75rem", paddingRight: "12px" }}
+                >
+                  {this.props.tableData[rowKey][column.property]}
                 </TableCell>
               ))}
             </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {Object.keys(this.props.tableData).map(rowKey => (
-              <TableRow
-                hover
-                onClick={e => this.props.handleRowClick(e.nativeEvent, rowKey)}
-                key={rowKey}
-                selected={this.props.selected.includes(rowKey)}
-                style={{ height: "36px" }}
-              >
-                {this.props.columns.map(column => (
-                  <TableCell
-                    key={rowKey + column.property}
-                    style={{ fontSize: "0.75rem", paddingRight: "12px" }}
-                  >
-                    {this.props.tableData[rowKey][column.property]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+          ))}
+        </TableBody>
+      </Table>
     );
   }
 }
