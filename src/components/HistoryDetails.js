7d649fe1795c839306815dd1e5535a24b23ed000
William Aperance <aperance@gmail.com>
2018-03-05 00:23:34 -0800

    Cleanup

--- src/components/HistoryDetails.js
+++ src/components/HistoryDetails.js
@@ -6,22 +6,20 @@ class HistoryDetails extends Component {
     return (
       <List>
         {this.props.values.map(value => (
-          <div>
-            <ListItem divider={true} key={value[0]}>
-              <div style={{ fontSize: "0.75rem", flex: "1 1 auto" }}>
-                <p style={{ margin: "0", minHeight: "14px" }}>{value[1]}</p>
-                <p
-                  style={{
-                    margin: "0",
-                    color: "darkgray",
-                    fontWeight: "300"
-                  }}
-                >
-                  {value[0]}
-                </p>
-              </div>
-            </ListItem>
-          </div>
+          <ListItem divider={true} key={value[0]}>
+            <div style={{ fontSize: "0.75rem", flex: "1 1 auto" }}>
+              <p style={{ margin: "0", minHeight: "14px" }}>{value[1]}</p>
+              <p
+                style={{
+                  margin: "0",
+                  color: "darkgray",
+                  fontWeight: "300"
+                }}
+              >
+                {value[0]}
+              </p>
+            </div>
+          </ListItem>
         ))}
       </List>
     );
