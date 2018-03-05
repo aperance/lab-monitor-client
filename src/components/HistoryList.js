1c9a23a7fe158c1d35122b57ba16a3a6818ff502
William Aperance <aperance@gmail.com>
2018-03-05 02:44:45 -0800

    Added react-virtualize to improve historyList rendering time

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,43 +1,42 @@
 import React, { Component } from "react";
-import List, { ListItem, ListItemText, ListItemIcon } from "material-ui/List";
+import { List } from "react-virtualized";
+import { ListItem, ListItemText, ListItemIcon } from "material-ui/List";
 import Icon from "material-ui/Icon";
 
 class HistoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { properties: this.props.properties.slice(0, 20) };
-    setTimeout(() => this.setState({ properties: this.props.properties }), 600);
-  }
-  componentWillReceiveProps(nextProps) {
-    this.setState({ properties: nextProps.properties });
-  }
   render() {
     return (
-      <List>
-        {this.state.properties.map(property => (
+      <List
+        width={600}
+        rowHeight={40}
+        height={window.innerHeight - 64}
+        rowCount={this.props.properties.length}
+        rowRenderer={({ key, index, isScrolling, isVisible, style }) => (
           <ListItem
+            key={key}
             button
             dense={true}
-            onClick={e => this.props.handleClick(property)}
             divider={true}
-            key={property}
-            style={
-              property === this.props.selectedProperty
-                ? { backgroundColor: "rgba(0, 0, 0, 0.04)" }
-                : null
-            }
+            style={{
+              ...style,
+              backgroundColor:
+                this.props.properties[index] === this.props.selectedProperty
+                  ? "rgba(0, 0, 0, 0.04)"
+                  : "white"
+            }}
+            onClick={e => this.props.handleClick(this.props.properties[index])}
           >
             <ListItemText
               disableTypography={true}
               style={{ fontSize: "0.75rem" }}
-              primary={property}
+              primary={this.props.properties[index]}
             />
             <ListItemIcon>
               <Icon style={{ marginRight: "0px" }}>navigate_next</Icon>
             </ListItemIcon>
           </ListItem>
-        ))}
-      </List>
+        )}
+      />
     );
   }
 }
