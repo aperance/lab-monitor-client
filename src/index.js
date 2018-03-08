589d0c94d5af15e92c614eb63f98dcf02b71de4c
William Aperance <aperance@gmail.com>
2018-03-08 02:52:46 -0800

    Added MUI theme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
-//import blue from "material-ui/colors/blue";
+import blue from "material-ui/colors/blue";
 import Layout from "./Layout";
 import store from "./store.js";
 import {} from "./socket.js";
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  palette: { primary: blue },
+  typography: { title: { fontWeight: 400 } }
+});
 
 ReactDOM.render(
   <Provider store={store}>
