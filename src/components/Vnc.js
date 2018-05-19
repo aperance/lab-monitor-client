d1becb6c1befa1327c57dc6345e73fd50684f825
William Aperance <aperance@gmail.com>
2018-05-18 23:04:22 -0700

    Added loading animation and error handling

--- src/components/Vnc.js
+++ src/components/Vnc.js
@@ -1,39 +1,61 @@
 import React, { Component } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import RFB from "../../node_modules/@novnc/novnc/core/rfb.js";
 
 class Vnc extends Component {
   constructor(props) {
     super(props);
     this.rfb = null;
+    this.state = { status: "pending" };
   }
 
   componentDidMount() {
     this.connectVnc();
   }
 
-  componentDidUpdate() {
-    this.rfb.disconnect();
-    this.connectVnc();
-  }
-
   componentWillUnmount() {
     this.rfb.disconnect();
-    console.log("vnc disconnected");
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.url !== prevProps.url) this.connectVnc();
   }
 
   connectVnc() {
-    console.log(this.props.url);
+    this.setState({ status: "pending" });
+    if (this.rfb) this.rfb.disconnect();
     if (this.props.url) {
       this.rfb = new RFB(document.getElementById("vnc"), this.props.url, {
         credentials: { password: this.props.password }
       });
       this.rfb.scaleViewport = true;
       this.rfb.resizeSession = true;
+      this.rfb.addEventListener("connect", e => {
+        this.setState({ status: "connected" });
+      });
+      this.rfb.addEventListener("disconnect", e => {
+        if (!e.detail.clean) {
+          this.setState({ status: "error" });
+          this.rfb = null;
+        }
+      });
     }
   }
 
   render() {
-    return <div id="vnc" style={{ height: "100%" }} />;
+    return (
+      <div style={{ height: "100%" }}>
+        {this.state.status === "pending" && <CircularProgress />}
+        {this.state.status === "error" && <p>Connection Failed</p>}
+        <div
+          id="vnc"
+          style={{
+            height: "100%",
+            visibility: this.state.status === "connected" ? "visible" : "hidden"
+          }}
+        />
+      </div>
+    );
   }
 }
 
