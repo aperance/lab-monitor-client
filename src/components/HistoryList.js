3b7a53a73969193d47e17304a4f242b122ff170a
William Aperance <aperance@gmail.com>
2018-06-03 23:35:14 -0700

    Added autosizing for history list

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { List } from "react-virtualized";
+import { List, AutoSizer } from "react-virtualized";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -16,35 +16,41 @@ const styles = theme => ({
 class HistoryList extends Component {
   render() {
     return (
-      <List
-        width={800}
-        rowHeight={40}
-        height={window.innerHeight - 64}
-        rowCount={this.props.properties.length}
-        rowRenderer={({ key, index, isScrolling, isVisible, style }) => (
-          <ListItem
-            button
-            style={style}
-            key={key}
-            dense={true}
-            divider={true}
-            className={
-              this.props.properties[index] !== this.props.selectedProperty
-                ? this.props.classes.row
-                : this.props.classes.selectedRow
-            }
-            onClick={e => this.props.handleClick(this.props.properties[index])}
-          >
-            <ListItemText
-              classes={{ primary: this.props.classes.text }}
-              primary={this.props.properties[index]}
-            />
-            <ListItemIcon className={this.props.classes.icon}>
-              <Icon>navigate_next</Icon>
-            </ListItemIcon>
-          </ListItem>
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <List
+            width={width}
+            rowHeight={40}
+            height={window.innerHeight - 64}
+            rowCount={this.props.properties.length}
+            rowRenderer={({ key, index, isScrolling, isVisible, style }) => (
+              <ListItem
+                button
+                style={style}
+                key={key}
+                dense={true}
+                divider={true}
+                className={
+                  this.props.properties[index] !== this.props.selectedProperty
+                    ? this.props.classes.row
+                    : this.props.classes.selectedRow
+                }
+                onClick={e =>
+                  this.props.handleClick(this.props.properties[index])
+                }
+              >
+                <ListItemText
+                  classes={{ primary: this.props.classes.text }}
+                  primary={this.props.properties[index]}
+                />
+                <ListItemIcon className={this.props.classes.icon}>
+                  <Icon>navigate_next</Icon>
+                </ListItemIcon>
+              </ListItem>
+            )}
+          />
         )}
-      />
+      </AutoSizer>
     );
   }
 }
