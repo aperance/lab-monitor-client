2fd660b7e616392bf21d164678109c6be1aa14fb
William Aperance <aperance@gmail.com>
2018-07-19 22:46:23 -0700

    Converted to typescript

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import * as React from "react";
 import NavBar from "./components/NavBar";
 import AssetTableContainer from "./containers/AssetTableContainer";
 import DrawersContainer from "./containers/DrawersContainer";
 import LogLevelContainer from "./containers/LogLevelContainer";
 import ActionResponseContainer from "./containers/ActionResponseContainer";
 
-class Layout extends Component {
-  render() {
+class Layout extends React.Component {
+  public render() {
     return (
       <div>
         <NavBar />
