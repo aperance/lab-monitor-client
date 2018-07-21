56473c04426b34992d8ea52899b92de712d7a49c
William Aperance <aperance@gmail.com>
2018-07-20 20:44:35 -0700

    Added device data validation. Cleanup

--- src/websocket.ts
+++ src/websocket.ts
@@ -26,14 +26,32 @@ enum MessageType {
   Error = "ERROR"
 }
 
+/**
+ *
+ */
 class Socket {
   private socket: WebSocket;
 
   constructor(url: string) {
     this.socket = new WebSocket(url);
-    this.start();
+
+    this.socket.addEventListener("open", () => {
+      console.log("websocket connected");
+      this.socket.addEventListener("message", message => {
+        this.messageHandler(message.data);
+      });
+    });
+
+    this.socket.addEventListener("close", () => {
+      console.log("websocket closed");
+      store.dispatch(resetAll());
+      // setTimeout(this.start.bind(this), 5000);
+    });
   }
 
+  /**
+   *
+   */
   public sendRefreshDevice(targets: string[]) {
     this.sendToServer({
       type: MessageType.RefreshDevice,
@@ -41,6 +59,9 @@ class Socket {
     });
   }
 
+  /**
+   *
+   */
   public sendDeviceAction(targets: string[], action: string, parameters = {}) {
     this.sendToServer({
       type: MessageType.DeviceAction,
@@ -48,6 +69,9 @@ class Socket {
     });
   }
 
+  /**
+   *
+   */
   public sendPsToolsCommand(
     target: string[],
     { mode, cmd }: { mode: string; cmd: string }
@@ -58,72 +82,97 @@ class Socket {
     });
   }
 
-  private start() {
-    this.socket.addEventListener("open", () => {
-      console.log("websocket connected");
-      this.socket.addEventListener("message", message => {
-        console.log(message.data);
-        const { type, payload } = JSON.parse(message.data);
-        if (
-          typeof type !== "string" ||
-          typeof payload !== "object" ||
-          payload === null
-        ) {
-          console.error("WS message received with unexpected structure.");
-          return;
-        }
-        switch (type) {
-          case MessageType.DeviceDataAll:
-            if (
-              typeof payload.state !== "object" ||
-              typeof payload.history !== "object"
-            )
-              console.error("Inavlid device data: " + payload);
-            else store.dispatch(deviceDataAll(payload.state, payload.history));
-            break;
-          case MessageType.DeviceDataUpdate:
-            if (
-              typeof payload.id !== "string" ||
-              typeof payload.state !== "object" ||
-              typeof payload.history !== "object"
-            )
-              console.error("Inavlid device data: " + payload);
-            else
-              store.dispatch(
-                deviceDataUpdate(payload.id, payload.state, payload.history)
-              );
-            break;
-          case MessageType.DeviceActionResponse:
-            if (
-              (payload.err !== null && payload.err !== Error) ||
-              payload.result === undefined
-            )
-              console.error("Inavlid device action response: " + payload);
-            else store.dispatch(actionResponseSet(payload.err, payload.result));
-            break;
-          case MessageType.PsToolsCommandResponse:
-            if (
-              (payload.err !== null && payload.err !== Error) ||
-              (payload.result !== null && typeof payload.result !== "string")
-            )
-              console.error("Inavlid psTools response: " + payload);
-            else store.dispatch(psToolsResponse(payload.err, payload.result));
-            break;
-          default:
-            console.error("Invalid WS message type specified");
-            break;
-        }
-        if (payload.err) console.log(payload.err);
+  /**
+   *
+   */
+  private messageHandler(message: any) {
+    console.log(message);
+    const { type, payload } = JSON.parse(message);
+    if (
+      typeof type !== "string" ||
+      typeof payload !== "object" ||
+      payload === null
+    ) {
+      console.error("WS message received with unexpected structure.");
+      return;
+    }
+    switch (type) {
+      case MessageType.DeviceDataAll:
+        this.deviceDataAllHandler(payload);
+        break;
+      case MessageType.DeviceDataUpdate:
+        this.deviceDataUpdateHandler(payload);
+        break;
+      case MessageType.DeviceActionResponse:
+        this.deviceActionResponseHandler(payload);
+        break;
+      case MessageType.PsToolsCommandResponse:
+        this.psToolsCommandResponseHandler(payload);
+        break;
+      default:
+        console.error("Invalid WS message type specified");
+        break;
+    }
+  }
+
+  /**
+   *
+   */
+  private deviceDataAllHandler(data: any) {
+    const isValid = Object.values(data).every(oneDeep => {
+      if (typeof oneDeep !== "object") return false;
+
+      return Object.values(oneDeep).every(twoDeep => {
+        if (typeof twoDeep !== "object") return false;
+
+        return Object.values(twoDeep).every(threeDeep => {
+          if (typeof threeDeep !== "string") return false;
+          else return true;
+        });
       });
     });
 
-    this.socket.addEventListener("close", () => {
-      console.log("websocket closed");
-      store.dispatch(resetAll());
-      setTimeout(this.start.bind(this), 5000);
-    });
+    if (isValid) store.dispatch(deviceDataAll(data.state, data.history));
+    else console.error("Invalid device data received.");
   }
 
+  /**
+   *
+   */
+  private deviceDataUpdateHandler(data: any) {
+    if (
+      typeof data.id !== "string" ||
+      typeof data.state !== "object" ||
+      typeof data.history !== "object"
+    )
+      console.error("Inavlid device data: " + data);
+    else store.dispatch(deviceDataUpdate(data.id, data.state, data.history));
+  }
+
+  /**
+   *
+   */
+  private deviceActionResponseHandler(data: any) {
+    if ((data.err !== null && data.err !== Error) || data.result === undefined)
+      console.error("Inavlid device action response: " + data);
+    else store.dispatch(actionResponseSet(data.err, data.result));
+  }
+
+  /**
+   *
+   */
+  private psToolsCommandResponseHandler(data: any) {
+    if (
+      (data.err !== null && data.err !== Error) ||
+      (data.result !== null && typeof data.result !== "string")
+    )
+      console.error("Inavlid psTools response: " + data);
+    else store.dispatch(psToolsResponse(data.err, data.result));
+  }
+
+  /**
+   *
+   */
   private sendToServer(message: IMessage) {
     this.socket.send(JSON.stringify(message));
   }
