6fb04e38d4cf80c36c9484bfdbb64f58eedb806d
William Aperance <aperance@gmail.com>
2018-07-21 13:58:48 -0700

    Moved logic from AssetTable to AssetTableContainer

--- src/components/AssetTable.js
+++ src/components/AssetTable.js
@@ -34,72 +34,25 @@ const styles = theme => ({
 });
 
 class AssetTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sort: {
-        by: null,
-        reverse: false
-      }
-    };
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // Use first column as default sort by property.
-    // Only set if not already set, and column data is known.
-    if (!prevState.sort.by && nextProps.columns[0])
-      return { sort: { by: nextProps.columns[0].property, reverse: false } };
-    else return null;
-  }
-
-  changeSorting(property) {
-    this.setState({
-      sort: {
-        by: property,
-        reverse:
-          this.state.sort.by === property ? !this.state.sort.reverse : false
-      }
-    });
-  }
-
-  sortAndFilter(tableData) {
-    return Object.entries(tableData)
-      .filter(([rowId, rowData]) =>
-        Object.entries(this.props.selectedFilters).every(
-          ([property, regexArray]) =>
-            !regexArray.every(regex => !rowData[property].match(regex)) ||
-            regexArray.length === 0
-        )
-      )
-      .sort((key1, key2) => {
-        const prop = this.state.sort.by;
-        let result = (key1[1][prop] || "") > (key2[1][prop] || "");
-        if (this.state.sort.reverse) result = !result;
-        return result ? 1 : -1;
-      });
-  }
-
   render() {
     return (
       <div className={this.props.classes.root}>
         <Table className={this.props.classes.table}>
           <AssetTableHead
             columns={this.props.columns}
-            sort={this.state.sort}
-            changeSorting={this.changeSorting.bind(this)}
+            sort={this.props.sort}
+            changeSorting={this.props.changeSorting}
           />
           <TableBody>
-            {this.sortAndFilter(this.props.tableData).map(
-              ([rowId, rowData]) => (
-                <AssetTableRow
-                  key={rowId}
-                  columns={this.props.columns}
-                  rowData={rowData}
-                  selected={this.props.selected.includes(rowId)}
-                  handleRowClick={e => this.props.handleRowClick(e, rowId)}
-                />
-              )
-            )}
+            {this.props.tableData.map(([rowId, rowData]) => (
+              <AssetTableRow
+                key={rowId}
+                columns={this.props.columns}
+                rowData={rowData}
+                selected={this.props.selected.includes(rowId)}
+                handleRowClick={e => this.props.handleRowClick(e, rowId)}
+              />
+            ))}
           </TableBody>
         </Table>
 
--- src/containers/AssetTableContainer.js
+++ src/containers/AssetTableContainer.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { singleRowSelect, multiRowSelect } from "../actions/actionCreators";
 import AssetTable from "../components/AssetTable";
@@ -20,7 +21,67 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
+class AssetTableContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sort: {
+        by: null,
+        reverse: false
+      }
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // Use first column as default sort by property.
+    // Only set if not already set, and column data is known.
+    if (!prevState.sort.by && nextProps.columns[0])
+      return { sort: { by: nextProps.columns[0].property, reverse: false } };
+    else return null;
+  }
+
+  changeSorting(property) {
+    this.setState({
+      sort: {
+        by: property,
+        reverse:
+          this.state.sort.by === property ? !this.state.sort.reverse : false
+      }
+    });
+  }
+
+  sortAndFilter(tableData) {
+    return Object.entries(tableData)
+      .filter(([rowId, rowData]) =>
+        Object.entries(this.props.selectedFilters).every(
+          ([property, regexArray]) =>
+            !regexArray.every(regex => !rowData[property].match(regex)) ||
+            regexArray.length === 0
+        )
+      )
+      .sort((key1, key2) => {
+        const prop = this.state.sort.by;
+        let result = (key1[1][prop] || "") > (key2[1][prop] || "");
+        if (this.state.sort.reverse) result = !result;
+        return result ? 1 : -1;
+      });
+  }
+
+  render() {
+    return (
+      <AssetTable
+        tableData={this.sortAndFilter(this.props.tableData)}
+        columns={this.props.columns}
+        selected={this.props.selected}
+        sort={this.state.sort}
+        changeSorting={this.changeSorting.bind(this)}
+        handleRowClick={this.props.handleRowClick.bind(this)}
+      />
+    );
+  }
+}
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(AssetTable);
+)(AssetTableContainer);
