63ecfd8ac3367223001186aa1bff9089e13f4650
William Aperance <aperance@gmail.com>
2018-10-15 01:13:51 -0700

    Removed props for log subview

--- src/containers/WebPageContainer.tsx
+++ src/containers/WebPageContainer.tsx
@@ -3,28 +3,13 @@ import { StoreState } from "../types";
 import WebPage from "../components/WebPage";
 
 const mapStateToProps = (state: StoreState) => {
-  switch (state.userSelection.view) {
-    case "statePage":
-      return {
-        url:
-          "http://" +
-          state.configuration.httpProxy +
-          state.configuration.statePath +
-          state.userSelection.rows[0]
-      };
-    case "logsPage":
-      return {
-        url:
-          "http://" +
-          state.configuration.httpProxy +
-          state.configuration.logsPath +
-          state.userSelection.rows[0]
-      };
-    default:
-      return {
-        url: null
-      };
-  }
+  return {
+    url:
+      "http://" +
+      state.configuration.httpProxy +
+      state.configuration.statePath +
+      state.userSelection.rows[0]
+  };
 };
 
 export default connect(mapStateToProps)(WebPage);
