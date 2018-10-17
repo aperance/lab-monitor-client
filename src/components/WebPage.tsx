b6da254f3e8b3a618462e82d973c430dedcbb35b
William Aperance <aperance@gmail.com>
2018-10-16 20:59:16 -0700

    Added "open in new tab" button

--- src/components/WebPage.tsx
+++ src/components/WebPage.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
 import { createStyles, WithStyles, withStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import OpenIcon from "@material-ui/icons/OpenInNew";
 
-const styles = createStyles({});
+const styles = createStyles({
+  button: {
+    position: "absolute",
+    right: "12px",
+    top: "12px",
+    backgroundColor: "white",
+    opacity: 0.5
+  }
+});
 
 interface Props extends WithStyles<typeof styles> {
   url: string | null;
@@ -11,16 +21,27 @@ class WebPage extends React.Component<Props> {
   public render() {
     return (
       this.props.url && (
-        <iframe
-          src={this.props.url}
-          id="qqq"
-          title="iframe"
-          width="100%"
-          height="100%"
-          style={{ borderWidth: "0px" }}
-        >
-          <p>Your browser does not support iframes.</p>
-        </iframe>
+        <>
+          <Button
+            variant="fab"
+            mini={true}
+            className={this.props.classes.button}
+            href={this.props.url}
+            target="_blank"
+          >
+            <OpenIcon />
+          </Button>
+          <iframe
+            src={this.props.url}
+            id="qqq"
+            title="iframe"
+            width="100%"
+            height="100%"
+            style={{ borderWidth: "0px" }}
+          >
+            <p>Your browser does not support iframes.</p>
+          </iframe>
+        </>
       )
     );
   }
