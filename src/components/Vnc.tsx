a3ab69ba36ba73521a132d4920db67fd93505252
William Aperance <aperance@gmail.com>
2018-10-21 04:58:13 -0700

    Split Vnc logic. Added 100ms pause to allow remote server to recover.

--- src/components/Vnc.tsx
+++ src/components/Vnc.tsx
@@ -4,69 +4,20 @@ import Button from "@material-ui/core/Button";
 import ExpandIcon from "@material-ui/icons/Fullscreen";
 import ShrinkIcon from "@material-ui/icons/FullscreenExit";
 import SaveIcon from "@material-ui/icons/GetApp";
-// @ts-ignore
-import RFB from "../../node_modules/@novnc/novnc/core/rfb";
 
 interface Props {
-  url: string | null;
-  password: string | null;
-}
-
-interface State {
-  status: string;
+  connectionState: string;
+  changeScale: () => void;
   scale: boolean;
+  target: any;
 }
 
-class Vnc extends React.Component<Props, State> {
-  public rfb: any = null;
-  public state: State = {
-    status: "pending",
-    scale: true
-  };
-
-  public componentDidMount() {
-    this.connectVnc();
-  }
-
-  public componentWillUnmount() {
-    if (this.rfb) this.rfb.disconnect();
-  }
-
-  public componentDidUpdate(prevProps: Props, prevState: State) {
-    if (this.props.url !== prevProps.url) this.connectVnc();
-    if (this.state.scale !== prevState.scale) this.connectVnc();
-    console.log("VNC UPDATED");
-  }
-
-  public connectVnc() {
-    this.setState({ status: "pending" });
-    if (this.rfb) this.rfb.disconnect();
-    if (this.props.url) {
-      this.rfb = new RFB(document.getElementById("vnc"), this.props.url, {
-        credentials: { password: this.props.password }
-      });
-      this.rfb.scaleViewport = this.state.scale;
-      this.rfb.resizeSession = true;
-      this.rfb.clipViewport = false;
-      this.rfb.dragViewport = false;
-      this.rfb.showDotCursor = true;
-      this.rfb.addEventListener("connect", () => {
-        this.setState({ status: "connected" });
-      });
-      this.rfb.addEventListener("disconnect", (e: any) => {
-        if (!e.detail.clean) {
-          this.setState({ status: "error" });
-          this.rfb = null;
-        }
-      });
-    }
-  }
-
+class Vnc extends React.Component<Props> {
   public render() {
     return (
       <div style={{ height: "100%" }}>
-        {this.state.status === "pending" && <LinearProgress />}
-        {this.state.status === "error" && (
+        {this.props.connectionState === "connecting" && <LinearProgress />}
+        {this.props.connectionState === "disconnected" && (
           <pre style={{ paddingLeft: "16px", color: "#f44336" }}>
             Connection Failed. Please ensure that VNC server is running on the
             remote device.
@@ -82,9 +33,9 @@ class Vnc extends React.Component<Props, State> {
             backgroundColor: "white",
             opacity: 0.5
           }}
-          onClick={() => this.setState({ scale: !this.state.scale })}
+          onClick={() => this.props.changeScale()}
         >
-          {this.state.scale ? <ExpandIcon /> : <ShrinkIcon />}
+          {this.props.scale ? <ExpandIcon /> : <ShrinkIcon />}
         </Button>
         <Button
           variant="fab"
@@ -105,12 +56,13 @@ class Vnc extends React.Component<Props, State> {
           <SaveIcon />
         </Button>
         <div
-          id="vnc"
           style={{
             height: "100%",
-            visibility: this.state.status === "connected" ? "visible" : "hidden"
+            visibility: "visible"
           }}
-        />
+        >
+          {this.props.target()}
+        </div>
       </div>
     );
   }
--- src/containers/VncContainer.tsx
+++ src/containers/VncContainer.tsx
@@ -1,6 +1,9 @@
+import * as React from "react";
 import { connect } from "react-redux";
 import { StoreState } from "../types";
 import Vnc from "../components/Vnc";
+// @ts-ignore
+import RFB from "../../node_modules/@novnc/novnc/core/rfb";
 
 const mapStateToProps = (state: StoreState) => {
   if (state.userSelection.rows.length === 1)
@@ -11,9 +14,65 @@ const mapStateToProps = (state: StoreState) => {
         state.userSelection.rows[0] +
         "&port=" +
         state.configuration.vnc.port,
-      password: state.configuration.vnc.password
+      password: state.configuration.vnc.password as string
     };
   else return { url: null, password: null };
 };
 
-export default connect(mapStateToProps)(Vnc);
+interface Props {
+  url: string | null;
+  password: string | null;
+}
+
+interface State {
+  scale: boolean;
+}
+
+class VncContainer extends React.Component<Props, State> {
+  public rfb: any = null;
+  public state: State = {
+    scale: true
+  };
+
+  public componentDidMount() {
+    this.connectVnc();
+  }
+
+  public componentWillUnmount() {
+    if (this.rfb && this.rfb._rfb_connection_state !== "disconnected")
+      this.rfb.disconnect();
+  }
+
+  public componentDidUpdate() {
+    this.connectVnc();
+  }
+
+  public connectVnc() {
+    if (this.rfb && this.rfb._rfb_connection_state !== "disconnected")
+      this.rfb.disconnect();
+    setTimeout(() => {
+      this.rfb = new RFB(document.getElementById("vnc"), this.props.url, {
+        credentials: { password: this.props.password }
+      });
+      this.rfb.scaleViewport = this.state.scale;
+      this.rfb.addEventListener("disconnect", (e: any) => {
+        if (!e.detail.clean) this.rfb = null;
+      });
+    }, 100);
+  }
+
+  public render() {
+    return (
+      <Vnc
+        connectionState={this.rfb && this.rfb._rfb_connection_state}
+        scale={this.state.scale}
+        changeScale={() => {
+          this.setState({ scale: !this.state.scale });
+        }}
+        target={() => <span id="vnc" />}
+      />
+    );
+  }
+}
+
+export default connect(mapStateToProps)(VncContainer);
