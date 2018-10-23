2ccd3f96732baa8844ede4570923a3736c1d1b51
William Aperance <aperance@gmail.com>
2018-10-23 02:16:05 -0700

    Improved reliability on reconnect

--- src/containers/VncContainer.tsx
+++ src/containers/VncContainer.tsx
@@ -31,12 +31,15 @@ interface Props {
 }
 
 interface State {
+  connected: boolean;
   scale: boolean;
 }
 
 class VncContainer extends React.Component<Props, State> {
   public rfb: any = null;
+  public timer?: any;
   public state: State = {
+    connected: false,
     scale: true
   };
 
@@ -44,33 +47,46 @@ class VncContainer extends React.Component<Props, State> {
     this.connectVnc();
   }
 
-  public componentWillUnmount() {
-    if (this.rfb && this.rfb._rfb_connection_state !== "disconnected")
-      this.rfb.disconnect();
+  public componentDidUpdate(prevProps: Props, prevSate: State) {
+    if (this.props.url !== prevProps.url) this.connectVnc();
+    if (this.state.scale !== prevSate.scale) this.connectVnc();
   }
 
-  public componentDidUpdate() {
-    this.connectVnc();
+  public componentWillUnmount() {
+    clearTimeout(this.timer);
+    this.disconnectVnc();
   }
 
   public connectVnc() {
-    if (this.rfb && this.rfb._rfb_connection_state !== "disconnected")
-      this.rfb.disconnect();
-    setTimeout(() => {
+    this.disconnectVnc();
+    this.timer = setTimeout(() => {
       this.rfb = new RFB(document.getElementById("vnc"), this.props.url, {
         credentials: { password: this.props.password }
       });
       this.rfb.scaleViewport = this.state.scale;
-      this.rfb.addEventListener("disconnect", (e: any) => {
-        if (!e.detail.clean) this.rfb = null;
-      });
+      this.rfb.addEventListener("connect", this.connectHandler);
+      this.rfb.addEventListener("disconnect", this.disconnectHandler);
     }, 100);
   }
+
+  public disconnectVnc() {
+    if (this.rfb) {
+      this.rfb.removeEventListener("connect", this.connectHandler);
+      this.rfb.removeEventListener("disconnect", this.disconnectHandler);
+      if (this.rfb._rfb_connection_state !== "disconnected")
+        this.rfb.disconnect();
+      this.rfb = null;
+    }
+  }
+
+  public connectHandler = () => this.setState({ connected: true });
+
+  public disconnectHandler = () => this.setState({ connected: false });
 
   public render() {
     return (
       <Vnc
-        connectionState={this.rfb && this.rfb._rfb_connection_state}
+        connected={this.state.connected}
         scale={this.state.scale}
         changeScale={() => {
           this.setState({ scale: !this.state.scale });
