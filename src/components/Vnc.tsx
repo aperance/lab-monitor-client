79e931dcedc60faf52b90a44109f1fc5c1ebe9ee
William Aperance <aperance@gmail.com>
2018-10-23 02:15:09 -0700

    Cleanup. Fixed visiblity and progress bar.

--- src/components/Vnc.tsx
+++ src/components/Vnc.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
+import { createStyles, WithStyles, withStyles } from "@material-ui/core";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Button from "@material-ui/core/Button";
 import ExpandIcon from "@material-ui/icons/Fullscreen";
 import ShrinkIcon from "@material-ui/icons/FullscreenExit";
 import SaveIcon from "@material-ui/icons/GetApp";
 
-interface Props {
-  connectionState: string;
+const styles = createStyles({
+  root: { height: "100%" },
+  buttonOne: {
+    position: "absolute",
+    right: "18px",
+    top: "18px",
+    backgroundColor: "white",
+    opacity: 0.5
+  },
+  buttonTwo: {
+    position: "absolute",
+    right: "18px",
+    top: "72px",
+    backgroundColor: "white",
+    opacity: 0.5
+  }
+});
+
+interface Props extends WithStyles<typeof styles> {
+  connected: boolean;
   changeScale: () => void;
   scale: boolean;
   target: any;
@@ -16,60 +35,40 @@ interface Props {
 class Vnc extends React.Component<Props> {
   public render() {
     return (
-      <div style={{ height: "100%" }}>
-        {this.props.connectionState === "connecting" && <LinearProgress />}
-        {this.props.connectionState === "disconnected" && (
-          <pre style={{ paddingLeft: "16px", color: "#f44336" }}>
-            Connection Failed. Please ensure that VNC server is running on the
-            remote device.
-          </pre>
-        )}
-        <Button
-          variant="fab"
-          mini={true}
-          style={{
-            position: "absolute",
-            right: "12px",
-            top: "12px",
-            backgroundColor: "white",
-            opacity: 0.5
-          }}
-          onClick={() => this.props.changeScale()}
-        >
-          {this.props.scale ? <ExpandIcon /> : <ShrinkIcon />}
-        </Button>
-        <Button
-          variant="fab"
-          mini={true}
-          style={{
-            position: "absolute",
-            right: "12px",
-            top: "64px",
-            backgroundColor: "white",
-            opacity: 0.5
-          }}
-          href={
-            this.props.fileContents &&
-            URL.createObjectURL(
-              new Blob([this.props.fileContents], { type: "text/plain" })
-            )
-          }
-          download="test.vnc"
-          target="_blank"
-        >
-          <SaveIcon />
-        </Button>
+      <>
+        {!this.props.connected && <LinearProgress />}
         <div
-          style={{
-            height: "100%",
-            visibility: "visible"
-          }}
+          className={this.props.classes.root}
+          style={{ visibility: this.props.connected ? "visible" : "hidden" }}
         >
           {this.props.target()}
+          <Button
+            variant="fab"
+            mini={true}
+            className={this.props.classes.buttonOne}
+            onClick={() => this.props.changeScale()}
+          >
+            {this.props.scale ? <ExpandIcon /> : <ShrinkIcon />}
+          </Button>
+          <Button
+            variant="fab"
+            mini={true}
+            className={this.props.classes.buttonTwo}
+            href={
+              this.props.fileContents &&
+              URL.createObjectURL(
+                new Blob([this.props.fileContents], { type: "text/plain" })
+              )
+            }
+            download="test.vnc"
+            target="_blank"
+          >
+            <SaveIcon />
+          </Button>
         </div>
-      </div>
+      </>
     );
   }
 }
 
-export default Vnc;
+export default withStyles(styles)(Vnc);
