33f69a5b73b37b1f5cc78487fc1da99fa5408767
William Aperance <aperance@gmail.com>
2018-10-29 21:48:40 -0700

    Refactored with hooks

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+// @ts-ignore
+import { useEffect } from "react";
 import { createStyles, WithStyles, withStyles } from "@material-ui/core";
 import { Collection, AutoSizer } from "react-virtualized";
 import HistoryItem from "./HistoryItem";
@@ -12,67 +14,59 @@ interface Props extends WithStyles<typeof styles> {
   handleClick: (property: string) => void;
 }
 
-class HistoryList extends React.Component<Props> {
-  public collectionRef: any = null;
+function HistoryList(props: Props) {
+  useEffect(() => recomputeCells());
 
-  public componentDidUpdate() {
-    this.recomputeCells();
-  }
+  let collectionRef: any = null;
+  const recomputeCells = () => {
+    if (collectionRef) collectionRef.recomputeCellSizesAndPositions();
+  };
 
-  public recomputeCells() {
-    if (this.collectionRef) this.collectionRef.recomputeCellSizesAndPositions();
-  }
-
-  public render() {
-    return (
-      <AutoSizer onResize={this.recomputeCells.bind(this)}>
-        {({ height, width }: any) => (
-          <Collection
-            ref={(ref: any) => (this.collectionRef = ref)}
-            style={{ position: "absolute", right: "0px" }} // overwritten if through class
-            height={height + 320}
-            width={width}
-            verticalOverscanSize={5}
-            cellCount={this.props.properties.length + 7}
-            cellRenderer={({
-              key,
-              index,
-              style
-            }: {
-              key: number;
-              index: number;
-              style: any;
-            }) => (
-              <HistoryItem
-                key={key}
-                style={style}
-                property={this.props.properties[index]}
-                isSelected={this.props.selectedIndex === index}
-                historyData={
-                  this.props.selectedIndex === index
-                    ? this.props.selectedData
-                    : null
-                }
-                handleClick={this.props.handleClick}
-              />
-            )}
-            cellSizeAndPositionGetter={({ index }: { index: number }) => {
-              const isSelected = this.props.selectedIndex === index;
-              const isBelowSelected =
-                this.props.selectedIndex !== -1 &&
-                index > this.props.selectedIndex;
-              return {
-                x: 0,
-                y: isBelowSelected ? 40 * (index + 7) : 40 * index,
-                width: width - 65,
-                height: isSelected ? 320 : 40
-              };
-            }}
-          />
-        )}
-      </AutoSizer>
-    );
-  }
+  return (
+    <AutoSizer onResize={recomputeCells}>
+      {({ height, width }: any) => (
+        <Collection
+          ref={(ref: any) => (collectionRef = ref)}
+          style={{ position: "absolute", right: "0px" }} // overwritten if through class
+          height={height + 320}
+          width={width}
+          verticalOverscanSize={5}
+          cellCount={props.properties.length + 7}
+          cellRenderer={({
+            key,
+            index,
+            style
+          }: {
+            key: number;
+            index: number;
+            style: any;
+          }) => (
+            <HistoryItem
+              key={key}
+              style={style}
+              property={props.properties[index]}
+              isSelected={props.selectedIndex === index}
+              historyData={
+                props.selectedIndex === index ? props.selectedData : null
+              }
+              handleClick={props.handleClick}
+            />
+          )}
+          cellSizeAndPositionGetter={({ index }: { index: number }) => {
+            const isSelected = props.selectedIndex === index;
+            const isBelowSelected =
+              props.selectedIndex !== -1 && index > props.selectedIndex;
+            return {
+              x: 0,
+              y: isBelowSelected ? 40 * (index + 7) : 40 * index,
+              width: width - 65,
+              height: isSelected ? 320 : 40
+            };
+          }}
+        />
+      )}
+    </AutoSizer>
+  );
 }
 
 export default withStyles(styles)(HistoryList);
