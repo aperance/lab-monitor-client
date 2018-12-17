d30b1fa6252e1c20a8b60a2a60a1ac0bfe544664
William Aperance <aperance@gmail.com>
2018-12-16 23:13:45 -0800

    Cleanup

--- src/hooks/useDataConditioner.ts
+++ src/hooks/useDataConditioner.ts
@@ -39,51 +39,48 @@ export const useDataConditioner = (rawData: RowData[]) => {
     reverse: false
   });
 
-  const conditionedData = rawData
-    .map(([id, rowData]) => {
-      columns
-        /** Filter out column config with no replacement rule */
-        .filter(x => typeof x.replace !== "undefined")
-        /** Iterate over column config with replacement rules */
-        .forEach(({ property, replace }) => {
-          if (replace && rowData[property] !== null) {
-            /** Apply all replacemnt rules on rowData */
-            Object.entries(replace).forEach(([replacement, matcher]) => {
-              if ((rowData[property] as string).match(matcher))
-                rowData[property] = replacement;
-            });
-          }
-        });
-      return [id, rowData] as [string, { [x: string]: string | null }];
-    })
-    .filter(([, rowData]) =>
-      Object.entries(selectedFilters).every(
-        ([property, regexArray]) =>
-          !regexArray.every(regex => !(rowData[property] || "").match(regex)) ||
-          regexArray.length === 0
-      )
-    )
-    .sort((key1: RowData, key2: RowData) => {
-      const prop =
-        key1[1][selectedSorting.property] !== key2[1][selectedSorting.property]
-          ? selectedSorting.property
-          : columns[0].property;
-      let result = (key1[1][prop] || "") > (key2[1][prop] || "");
-      if (selectedSorting.reverse) result = !result;
-      return result ? 1 : -1;
-    });
+  const replaceFunction = ([id, rowData]: RowData): RowData => {
+    columns
+      /** Filter out column config with no replacement rule */
+      .filter(x => typeof x.replace !== "undefined")
+      /** Iterate over column config with replacement rules */
+      .forEach(({ property, replace }) => {
+        if (replace && rowData[property] !== null) {
+          /** Apply all replacemnt rules on rowData */
+          Object.entries(replace).forEach(([replacement, matcher]) => {
+            if ((rowData[property] as string).match(matcher))
+              rowData[property] = replacement;
+          });
+        }
+      });
+    return [id, rowData];
+  };
+
+  const filterFunction = ([, rowData]: RowData): boolean =>
+    Object.entries(selectedFilters).every(
+      ([property, regexArray]) =>
+        !regexArray.every(regex => !(rowData[property] || "").match(regex)) ||
+        regexArray.length === 0
+    );
+
+  const sortFunction = (key1: RowData, key2: RowData): 1 | -1 => {
+    const prop =
+      key1[1][selectedSorting.property] !== key2[1][selectedSorting.property]
+        ? selectedSorting.property
+        : columns[0].property;
+    let result = (key1[1][prop] || "") > (key2[1][prop] || "");
+    if (selectedSorting.reverse) result = !result;
+    return result ? 1 : -1;
+  };
 
   return [
-    conditionedData,
+    rawData
+      .map(replaceFunction)
+      .filter(filterFunction)
+      .sort(sortFunction),
     selectedFilters,
-    (property, regex) => filterDispatch({ property, regex }),
+    (property: string, regex: string) => filterDispatch({ property, regex }),
     selectedSorting,
-    property => sortDispatch({ property })
-  ] as [
-    RowData[],
-    { [x: string]: string[] },
-    (x: string, y: string) => void,
-    { property: string; reverse: boolean },
-    (x: string) => void
+    (property: string) => sortDispatch({ property })
   ];
 };
