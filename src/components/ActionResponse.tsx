0b630338e82efe9c9f96b7a2db7e4ba47b01048b
William Aperance <aperance@gmail.com>
2020-05-18 22:36:30 -0700

    Display error on demo mode

--- src/components/ActionResponse.tsx
+++ src/components/ActionResponse.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
-import { Snackbar, IconButton } from "@material-ui/core";
+import {Snackbar, IconButton} from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 
 interface Props {
-  response: { err: Error | null; results: any[] | null };
+  response: {err: Error | null; results: any[] | null};
   handleClose: () => void;
 }
 
 const ActionResponse = (props: Props) => {
   let message: string | null = null;
 
-  if (props.response.err !== null)
-    message = "ERROR: " + props.response.err.message;
+  if (props.response.err !== null) message = "ERROR: " + props.response.err;
   else if (props.response.results !== null)
     message = props.response.results.every(result => result.success === true)
       ? "Request successfuly received by device(s)."
