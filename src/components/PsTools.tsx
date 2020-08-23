7310ef129355156e7d2cda5afcdfabfd34ae7356
William Aperance <aperance@gmail.com>
2020-08-22 18:52:47 -0700

    Merged terminal into patools component

--- src/components/PsTools.tsx
+++ src/components/PsTools.tsx
@@ -11,6 +11,7 @@ import {
   FormControl,
   Select,
   Button,
+  Paper,
   makeStyles
 } from "@material-ui/core";
 
@@ -19,7 +20,6 @@ import {WebsocketContext} from "../websockets/WebsocketContext";
 import {psToolsRequest} from "../websockets/messageCreators";
 import {useSelector, useDispatch} from "../redux/store";
 import {psToolsResponseClear} from "../redux/actionCreators";
-import Terminal from "./Terminal";
 
 /**  CSS-in-JS styling */
 const useStyles = makeStyles({
@@ -33,102 +33,111 @@ const useStyles = makeStyles({
   presetsInput: {width: "100%"},
   modeInput: {width: "76px", marginRight: "16px"},
   cmdInput: {width: "calc(100% - 164px)", marginRight: "8px"},
-  text: {fontSize: "0.825rem"}
+  text: {fontSize: "0.825rem"},
+  paper: {
+    height: "500px",
+    width: "100%",
+    marginTop: "32px",
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "0.825rem",
+    "& pre": {padding: "10px 10px 10px 10px", whiteSpace: "pre-wrap"}
+  }
 });
 
-const PsTools = (): JSX.Element => {
-  /** Generated CSS class names */
-  const classes = useStyles();
-  /** IP Address of target device */
-  const ipAddress = useSelector(state => state.userSelection.rows[0] ?? null);
-  /** Output from executed PSTools command  */
-  const result = useSelector(state => state.deviceResponse.psTools);
-  const dispatch = useDispatch();
-  /** Function to send message via Websocket */
-  const {send} = useContext(WebsocketContext);
+const PsTools = (): JSX.Element | null => {
   const [preset, setPreset] = useState("");
   const [mode, setMode] = useState("");
   const [cmd, setCmd] = useState("");
+  /** Generated CSS class names */
+  const classes = useStyles();
+  /** Function to send message via Websocket */
+  const {send} = useContext(WebsocketContext);
+  /** IP Address of target device */
+  const ipAddress = useSelector(state => state.userSelection.rows[0] || null);
+  /** Output from executed PSTools command  */
+  const result = useSelector(state => state.deviceResponse.psTools);
+  const dispatch = useDispatch();
 
   const presets = config.psTools;
 
+  if (ipAddress === null) return null;
+
   return (
-    <>
-      {ipAddress && (
-        <div className={classes.container}>
-          {presets && (
-            <form>
-              <FormControl className={classes.presetsInput}>
-                <InputLabel>Load Preset Command</InputLabel>
-                <Select
-                  className={classes.text}
-                  input={<Input id="presets" />}
-                  value={preset}
-                  onChange={e => {
-                    const value = e.target.value as string;
-                    setPreset(value);
-                    setMode(presets[value].mode);
-                    setCmd(presets[value].cmd);
-                  }}
-                >
-                  {Object.keys(presets).map(x => (
-                    <MenuItem className={classes.text} key={x} value={x}>
-                      {presets[x].name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </form>
-          )}
-
-          <form>
-            <FormControl className={classes.modeInput}>
-              <InputLabel>Mode</InputLabel>
-              <Select
-                className={classes.text}
-                input={<Input id="mode" />}
-                value={mode}
-                onChange={e => {
-                  const value = e.target.value as string;
-                  setPreset("");
-                  setMode(value);
-                }}
-              >
-                <MenuItem className={classes.text} value="psExec">
-                  PSExec
-                </MenuItem>
-                <MenuItem className={classes.text} value="psKill">
-                  PSKill
-                </MenuItem>
-              </Select>
-            </FormControl>
-
-            <FormControl className={classes.cmdInput}>
-              <InputLabel htmlFor="name-input">Command</InputLabel>
-              <Input
-                id="cmd"
-                value={cmd}
-                onChange={e => {
-                  setPreset("");
-                  setCmd(e.target.value);
-                }}
-              />
-            </FormControl>
-
-            <Button
-              size="small"
-              onClick={() => {
-                dispatch(psToolsResponseClear());
-                if (ipAddress) send(psToolsRequest(ipAddress, mode, cmd));
+    <div className={classes.container}>
+      {presets && (
+        <form>
+          <FormControl className={classes.presetsInput}>
+            <InputLabel>Load Preset Command</InputLabel>
+            <Select
+              className={classes.text}
+              input={<Input id="presets" />}
+              value={preset}
+              onChange={e => {
+                const value = e.target.value as string;
+                setPreset(value);
+                setMode(presets[value].mode);
+                setCmd(presets[value].cmd);
               }}
             >
-              Send
-            </Button>
-          </form>
-          <Terminal output={result} />
-        </div>
+              {Object.keys(presets).map(x => (
+                <MenuItem className={classes.text} key={x} value={x}>
+                  {presets[x].name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </form>
       )}
-    </>
+
+      <form>
+        <FormControl className={classes.modeInput}>
+          <InputLabel>Mode</InputLabel>
+          <Select
+            className={classes.text}
+            input={<Input id="mode" />}
+            value={mode}
+            onChange={e => {
+              const value = e.target.value as string;
+              setPreset("");
+              setMode(value);
+            }}
+          >
+            <MenuItem className={classes.text} value="psExec">
+              PSExec
+            </MenuItem>
+            <MenuItem className={classes.text} value="psKill">
+              PSKill
+            </MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl className={classes.cmdInput}>
+          <InputLabel htmlFor="name-input">Command</InputLabel>
+          <Input
+            id="cmd"
+            value={cmd}
+            onChange={e => {
+              setPreset("");
+              setCmd(e.target.value);
+            }}
+          />
+        </FormControl>
+
+        <Button
+          size="small"
+          onClick={() => {
+            dispatch(psToolsResponseClear());
+            if (ipAddress) send(psToolsRequest(ipAddress, mode, cmd));
+          }}
+        >
+          Send
+        </Button>
+      </form>
+      <Paper className={classes.paper} elevation={8} square={true}>
+        {result && <pre>{result}</pre>}
+      </Paper>
+    </div>
   );
 };
 
