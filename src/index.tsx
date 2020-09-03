05b948ef4c477cad325eaaf891bba69aba1c21e5
William Aperance <aperance@gmail.com>
2020-09-03 02:41:58 -0700

    Prettier formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {MuiThemeProvider, createMuiTheme} from "@material-ui/core/styles";
-import {blue} from "@material-ui/core/colors";
+import { Provider } from "react-redux";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
 
-import {WebsocketProvider} from "./websockets/WebsocketContext";
+import { WebsocketProvider } from "./WebsocketContext";
 import store from "./redux/store";
 import App from "./components/App";
 
 const theme = createMuiTheme({
-  palette: {primary: blue}
+  palette: { primary: blue }
 });
 
 ReactDOM.render(
